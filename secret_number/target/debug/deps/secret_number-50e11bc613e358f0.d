547008ad049c78fa9acb4e3c1197e32ebe2a10ac
wicje <anichisom4top@gmail.com>
2025-02-12 00:43:59 +0100

    ok

---
+++ secret_number/target/debug/deps/secret_number-50e11bc613e358f0.d
@@ -0,0 +1,5 @@
+/home/josephan/development/rust_folder/secret_number/target/debug/deps/libsecret_number-50e11bc613e358f0.rmeta: src/main.rs
+
+/home/josephan/development/rust_folder/secret_number/target/debug/deps/secret_number-50e11bc613e358f0.d: src/main.rs
+
+src/main.rs:
---
+++ secret_number/target/debug/deps/secret_number-eb80279a5d1f48f6.d
@@ -0,0 +1,5 @@
+/home/josephan/development/rust_folder/secret_number/target/debug/deps/libsecret_number-eb80279a5d1f48f6.rmeta: src/main.rs
+
+/home/josephan/development/rust_folder/secret_number/target/debug/deps/secret_number-eb80279a5d1f48f6.d: src/main.rs
+
+src/main.rs:
